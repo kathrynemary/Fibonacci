b1257fec161c50a4066ca98ec480fe64015a9d73
Kathryne Dunlap <kathryne@Kathrynes-MacBook-Pro.local>
2016-02-05 09:42:49 -0600

    Is actually accurate now

--- Sequence.rb
+++ Sequence.rb
@@ -1,10 +1,8 @@
 class Fibonacci
 
 	def fibonaccify(iterations)
-		if iterations == 0
+		if iterations == 0 || iterations == 1
 			answer = 1
-		elsif iterations == 1
-			answer = 2
 		elsif iterations > 1
 			a = fibonaccify(iterations - 1)
 			b = fibonaccify(iterations - 2)
--- SequenceTest.rb
+++ SequenceTest.rb
@@ -3,33 +3,43 @@ require './Sequence'
 
 class TestSequence < Minitest::Test
 
-	def test_sum_of_1_iteration
-		x = Fibonacci.new.fibonaccify(1)
-		assert_equal 2, x
+	def test_sum_of_1st_iteration
+		x = Fibonacci.new.fibonaccify(0)
+		assert_equal 1, x
 	end
 
 	def test_sum_of_2nd_iteration
-		x = Fibonacci.new.fibonaccify(2)
-		assert_equal 3, x
+		x = Fibonacci.new.fibonaccify(1)
+		assert_equal 1, x
 	end
 
 	def test_sum_of_3rd_iteration
-		x = Fibonacci.new.fibonaccify(3)
-		assert_equal 5, x
+		x = Fibonacci.new.fibonaccify(2)
+		assert_equal 2, x
 	end
 
 	def test_sum_of_4th_iteration
-		x = Fibonacci.new.fibonaccify(4)
-		assert_equal 8, x
+		x = Fibonacci.new.fibonaccify(3)
+		assert_equal 3, x
 	end
 
 	def test_sum_of_5th_iteration
-		x = Fibonacci.new.fibonaccify(5)
-		assert_equal 13, x
+		x = Fibonacci.new.fibonaccify(4)
+		assert_equal 5, x
 	end
 
 	def test_sum_of_6th_iteration
+		x = Fibonacci.new.fibonaccify(5)
+		assert_equal 8, x
+	end
+
+	def test_sum_of_7th_iteration
 		x = Fibonacci.new.fibonaccify(6)
+		assert_equal 13, x
+	end
+
+	def test_sum_of_8th_iteration
+		x = Fibonacci.new.fibonaccify(7)
 		assert_equal 21, x
 	end
 
